8e6de10da08e0fb6cb25ff119a6c3fe168bfd91d
Pereira Oliveira Lima <epereirao@indracompany.com>
2020-08-02 17:24:40 -0300

    Usando componente Link, para não dar load todal.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg'
@@ -15,10 +16,10 @@ export default function Logon(){
                     <input placeholder="Sua ID" />
                     <button type="submit" className="button">Entrar</button>
 
-                    <a href="/register">
+                    <Link to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes" />
