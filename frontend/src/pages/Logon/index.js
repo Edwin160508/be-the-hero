e53db34918cade0244d37f8acfc47a4dec02487c
Edwin Lima <edwin.limaa@gmail.com>
2020-08-12 22:04:44 -0300

    Relizando login, Profile usando localStorage.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
+import api from '../../services/api';
 
 export default function Logon(){
+    const [id, setId ] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(event){
+        event.preventDefault();
+        try{            
+            const response = await api.post('session', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name); 
+            
+            history.push('/profile');
+        }catch(err){
+            console.log(err);
+            alert("Falha no login, tente novamente.");
+        }
+    }
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Heroe" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e=> setId(e.target.value)}
+                        />
                     <button type="submit" className="button">Entrar</button>
 
                     <Link className="back-link" to="/register">
