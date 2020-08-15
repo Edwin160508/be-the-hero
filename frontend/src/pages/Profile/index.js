daaac28488c89d4aa9708457f381f086216bcd46
Edwin Lima <edwin.limaa@gmail.com>
2020-08-15 18:22:33 -0300

    Criando a funcionalidade logout da aplicação.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
@@ -7,6 +7,7 @@ import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
 export default function Profile() {
+    const history = useHistory();
     const [incidents, setIncidents] = useState([]);
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
@@ -37,6 +38,12 @@ export default function Profile() {
             alert('Erro ao deletar caso, tente novamente ou fale com administrador.');
         }
     }
+
+    function handleLogout(){        
+       localStorage.clear();
+       history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -44,7 +51,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
